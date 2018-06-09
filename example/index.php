d763c6b93ca43c08f6b1a57f435e2dcf895845f1
Michael Schwarz <089michael@gmail.com>
2018-06-09 16:05:50 +0200

    Add example file

---
+++ example/index.php
@@ -0,0 +1,22 @@
+<?php
+require_once './../vendor/autoload.php';
+
+use YoHang88\LetterAvatar\LetterAvatar;
+
+$avatar = new LetterAvatar('Max Mustermann', 'square', 64);
+
+?>
+
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test</title>
+</head>
+<body>
+<img src="<?php echo $avatar;?>" alt="">
+</body>
+</html>
