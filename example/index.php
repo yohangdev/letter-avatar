b283570626af8f7a294eb32bef966bdb7d065693
Michael Schwarz <089michael@gmail.com>
2018-06-09 16:59:29 +0200

    Change example

--- example/index.php
+++ example/index.php
@@ -3,20 +3,19 @@ require_once './../vendor/autoload.php';
 
 use YoHang88\LetterAvatar\LetterAvatar;
 
-$avatar = new LetterAvatar('Max Mustermann', 'square', 64);
-$avatar->saveAs('./img.png')
+$avatar = new LetterAvatar('Max Mustermann', 'circle', 64);
 ?>
 
 <!doctype html>
-<html lang="de">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Test</title>
+    <title>Example</title>
 </head>
 <body>
-<img src="<?php echo $avatar;?>" alt="">
+    <img src="<?php echo $avatar;?>" alt="">
 </body>
 </html>
